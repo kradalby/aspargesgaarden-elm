cff44cf187c0416068beda11c945b63fa4d29311
Kristoffer Dalby <kradalby@kradalby.no>
2021-12-30 17:32:50 +0100

    Move about to markdown

--- src/Page/About.elm
+++ src/Page/About.elm
@@ -6,6 +6,10 @@ import Head
 import Head.Seo as Seo
 import Html.Styled exposing (..)
 import Html.Styled.Attributes exposing (alt, attribute, css, for, href, id, src, type_)
+import List exposing (tail)
+import Markdown.Parser
+import Markdown.Renderer
+import MarkdownCodec
 import Page exposing (Page, PageWithState, StaticPayload)
 import Pages.PageUrl exposing (PageUrl)
 import Pages.Url
@@ -13,6 +17,7 @@ import Shared
 import String.Format
 import Tailwind.Breakpoints as Bp
 import Tailwind.Utilities as Tw
+import TailwindMarkdownRenderer
 import View exposing (View)
 import View.Misc exposing (contact, container, headline, paragraph)
 
@@ -73,6 +78,50 @@ view :
     -> StaticPayload Data RouteParams
     -> View Msg
 view maybeUrl sharedModel static =
+    let
+        markdown =
+            """
+Aspargesgården byr på sjarmerende og unike lokaler i landlige omgivelser, ved kysten i Vestfold.
+
+Vi byr på annerledes og stemningsfylte selskapslokaler, hvor vi ønsker velkommen til alle typer selskap; bryllup, konfirmasjoner, dåp, minnestunder, bursdager, firmasamlinger, kurs , konserter og konferanser.
+
+Låven og fjøset rommer det dere trenger av moderne teknisk utstyr, samt komfortable fasiliteter og fremkommelighet for alle! Vi har det du trenger til å dekke et vakkert festbord og skape den rette stemning!
+
+Her hos oss kan du velge om du vil gjøre litt- mye- eller ingenting selv!
+
+Ta en nærmere kikk på [muligheter om hva vi kan tilby](/facilities) - og skulle du ha spesielle ønsker; ikke nøl med å ta kontakt! La oss skape den perfekte rammen for ditt arrangement!
+
+
+
+# Vertskapet
+
+I 2014 flyttet vi fra Sandefjord til idylliske Tjølling og Aspargesgården. Tidligere eiere dyrket asparges her, og gjorde om fjøset til selskapslokaler, derav navnet «Aspargesgården»
+
+Vi -  min mann Anders og jeg, trengte låven til kurs, møter og lager for vår salgsstyrke og Tupperware produkter.
+
+Vi har tre voksne barn, og lille Angel vår søte Bichon Havanais.
+
+Jeg har alltid hatt en drøm om å bo på gård, selv om jeg er en skikkelig «byjente». Elsker gamle hus, historie, interiør, hage og å gjøre det vakkert rundt meg. Anders er «handyman» som restaurerer og bygger, og sammen utvikler vi gården kontinuerlig.
+
+Det er en utrolig glede og skape så mange fantastiske øyeblikk, muligheter og opplevelser.
+
+Vi ser frem til å ønske mange brudepar, selskap, og alle andre som har lyst til å gjøre noe morsomt her på gården velkommen!
+
+Alt godt, Kristine & Anders
+        """
+
+        deadEndsToString deadEnds =
+            deadEnds
+                |> List.map Markdown.Parser.deadEndToString
+                |> String.join "\n"
+
+        content =
+            markdown
+                |> Markdown.Parser.parse
+                |> Result.mapError deadEndsToString
+                |> Result.andThen (\ast -> Markdown.Renderer.render TailwindMarkdownRenderer.renderer ast)
+                |> Result.withDefault [ text "Kunne ikke laste innhold..." ]
+    in
     View.html "Om oss"
         [ container
             [ div [] [ headline "Om gården" ]
@@ -91,23 +140,10 @@ view maybeUrl sharedModel static =
                         , Tw.text_brown_text
                         ]
                     ]
-                    [ paragraph "Aspargesgården byr på sjarmerende og unike lokaler i landlige omgivelser, ved kysten i Vestfold."
-                    , paragraph "Vi byr på annerledes og stemningsfylte selskapslokaler, hvor vi ønsker velkommen til alle typer selskap; bryllup, konfirmasjoner, dåp, minnestunder, bursdager, firmasamlinger, kurs , konserter og konferanser."
-                    , paragraph "Låven og fjøset rommer det dere trenger av moderne teknisk utstyr, samt komfortable fasiliteter og fremkommelighet for alle! Vi har det du trenger til å dekke et vakkert festbord og skape den rette stemning!"
-                    , paragraph "Her hos oss kan du velge om du vil gjøre litt- mye- eller ingenting selv!"
-                    , paragraph "Ta en nærmere kikk på muligheter om hva vi kan tilby - og skulle du ha spesielle ønsker; ikke nøl med å ta kontakt! La oss skape den perfekte rammen for ditt arrangement!"
-                    , br [] []
-                    , br [] []
-                    , headline "Vertskap"
-                    , paragraph "I 2014 flyttet vi fra Sandefjord til idylliske Tjølling og Aspargesgården. Tidligere eiere dyrket asparges her, og gjorde om fjøset til selskapslokaler, derav navnet «Aspargesgården»"
-                    , paragraph "Vi -  min mann Anders og jeg, trengte låven til kurs, møter og lager for vår salgsstyrke og Tupperware produkter."
-                    , paragraph "Vi har tre voksne barn, og lille Angel vår søte Bichon Havanais."
-                    , paragraph "Jeg har alltid hatt en drøm om å bo på gård, selv om jeg er en skikkelig «byjente». Elsker gamle hus, historie, interiør, hage og å gjøre det vakkert rundt meg. Anders er «handyman» som restaurerer og bygger, og sammen utvikler vi gården kontinuerlig."
-                    , paragraph "Det er en utrolig glede og skape så mange fantastiske øyeblikk, muligheter og opplevelser."
-                    , paragraph "Vi ser frem til å ønske mange brudepar, selskap, og alle andre som har lyst til å gjøre noe morsomt her på gården velkommen!"
-                    , paragraph "Alt godt, Kristine & Anders"
-                    , contact
-                    ]
+                  <|
+                    content
+                        ++ [ contact
+                           ]
                 , div
                     [ css
                         [ Bp.md [ Tw.w_1over2, Tw.neg_mt_12 ]
