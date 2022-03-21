ac62996ae036b83b7170d0a1be677df37fdcc891
Kristoffer Dalby <kradalby@kradalby.no>
2022-03-21 16:34:37 +0000

    Add resize script

---
+++ resize_images.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env sh
+
+for width in 320 640 768 1024 1280 1536 2048; do
+	fd -e jpg -E "*resize*" -x convert {} -resize "${width}x>" {.}"_${width}w_resize.jpg"
+done
+
+./node_modules/.bin/optimizt --avif --webp public/
