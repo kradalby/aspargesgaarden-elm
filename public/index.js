a6977852f29c36103bfe2114a1ce6f58befe4da6
Kristoffer Dalby <kradalby@kradalby.no>
2022-03-24 20:55:28 +0000

    Generate resized images and modrn formats

--- public/index.js
+++ public/index.js
@@ -1,5 +1,7 @@
 /** @typedef {{load: (Promise<unknown>); flags: (unknown)}} ElmPagesInit */
 
+import "/imgsupport.js";
+
 /** @type ElmPagesInit */
 export default {
   load: async function (elmLoaded) {
