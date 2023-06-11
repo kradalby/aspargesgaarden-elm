ba7556953c447927c90cba321c7fc98712f2bd3d
Kristoffer Dalby <kristoffer@dalby.cc>
2023-06-11 11:51:17 +0200

    upgrade deps and move to nix build
    Signed-off-by: Kristoffer Dalby <kristoffer@dalby.cc>

---
+++ elm-srcs.nix
@@ -0,0 +1,212 @@
+{
+
+      "MartinSStewart/elm-serialize" = {
+        sha256 = "1nz2f8y7w6s6w8vnb591ri0kibr9n4p93n4sbwz0vvxljp3zah67";
+        version = "1.3.0";
+      };
+
+      "MaybeJustJames/yaml" = {
+        sha256 = "1505z4nw199lbnggy93b6xn1w1y0ncqfz6drwrvmpp51xjca5zls";
+        version = "2.1.3";
+      };
+
+      "danyx23/elm-mimetype" = {
+        sha256 = "0bj904nzdj9xpwi50f0rqm1had3kziqbn9fh68z0hkc2psppchcc";
+        version = "4.0.1";
+      };
+
+      "dillonkearns/elm-bcp47-language-tag" = {
+        sha256 = "1rl9cw10aa7q7wlw6smp65vn2lj5alsa08w35qas54q5jv0hqnqr";
+        version = "1.0.1";
+      };
+
+      "dillonkearns/elm-markdown" = {
+        sha256 = "0gmajcqih4fam4ydjkc10lz4sh9rig1vq2k1jz748jijwngbbhha";
+        version = "7.0.1";
+      };
+
+      "dillonkearns/elm-pages" = {
+        sha256 = "16mw3h9hmmnyx7pqgn6abvay3hlm114n0xalln4y55yxkdynbgbd";
+        version = "9.0.0";
+      };
+
+      "elm-community/list-extra" = {
+        sha256 = "02grd0p5hc2gvdy4n723d1s28pm1grn95jrzic6jcgb26qh16vcc";
+        version = "8.7.0";
+      };
+
+      "elm/browser" = {
+        sha256 = "0nagb9ajacxbbg985r4k9h0jadqpp0gp84nm94kcgbr5sf8i9x13";
+        version = "1.0.2";
+      };
+
+      "elm/core" = {
+        sha256 = "19w0iisdd66ywjayyga4kv2p1v9rxzqjaxhckp8ni6n8i0fb2dvf";
+        version = "1.0.5";
+      };
+
+      "elm/html" = {
+        sha256 = "1n3gpzmpqqdsldys4ipgyl1zacn0kbpc3g4v3hdpiyfjlgh8bf3k";
+        version = "1.0.0";
+      };
+
+      "elm/json" = {
+        sha256 = "0kjwrz195z84kwywaxhhlnpl3p251qlbm5iz6byd6jky2crmyqyh";
+        version = "1.1.3";
+      };
+
+      "elm/url" = {
+        sha256 = "0av8x5syid40sgpl5vd7pry2rq0q4pga28b4yykn9gd9v12rs3l4";
+        version = "1.0.0";
+      };
+
+      "jorgengranseth/elm-string-format" = {
+        sha256 = "1df8ck2saqzr4nak8zj643arxg9gdw22lywnswril9bzhlg2q30r";
+        version = "1.0.1";
+      };
+
+      "pablohirafuji/elm-syntax-highlight" = {
+        sha256 = "1bs1xhj1ncd98j4fb06rnhspgy4q1wfz1c85rc51lri0cgkwdabg";
+        version = "3.4.1";
+      };
+
+      "rtfeldman/elm-css" = {
+        sha256 = "0mffazjks3wmsn0f0xs8gs6zg39ahipz0hqif2y7qphzjdq6h6yh";
+        version = "17.1.1";
+      };
+
+      "avh4/elm-color" = {
+        sha256 = "0n16wnvp87x9az3m5qjrl6smsg7051m719xn5d244painx8xmpzq";
+        version = "1.0.0";
+      };
+
+      "bburdette/toop" = {
+        sha256 = "10i3j97pzc903n0rwkxna6g1pf3lg4zipgrg6r2glygzazaigbs7";
+        version = "1.2.0";
+      };
+
+      "danfishgold/base64-bytes" = {
+        sha256 = "1xw4kq7r4zpx484wcnwwkcgzmr4wdxsv3i0j90ynnrjc68ydwgj1";
+        version = "1.1.0";
+      };
+
+      "elm-community/dict-extra" = {
+        sha256 = "05ll04wf03m8ic109dz2dbq6pah23m70c4wwyr35026dhmws35n0";
+        version = "2.4.0";
+      };
+
+      "elm-explorations/test" = {
+        sha256 = "0x2vjclcnka83awn4846cp78qkd9x4s3n6sjrvrirdiixahfm6g1";
+        version = "2.1.1";
+      };
+
+      "elm/bytes" = {
+        sha256 = "02ywbf52akvxclpxwj9n04jydajcbsbcbsnjs53yjc5lwck3abwj";
+        version = "1.0.8";
+      };
+
+      "elm/file" = {
+        sha256 = "1rljcb41dl97myidyjih2yliyzddkr2m7n74x7gg46rcw4jl0ny8";
+        version = "1.0.5";
+      };
+
+      "elm/http" = {
+        sha256 = "008bs76mnp48b4dw8qwjj4fyvzbxvlrl4xpa2qh1gg2kfwyw56v1";
+        version = "2.0.0";
+      };
+
+      "elm/parser" = {
+        sha256 = "0a3cxrvbm7mwg9ykynhp7vjid58zsw03r63qxipxp3z09qks7512";
+        version = "1.1.0";
+      };
+
+      "elm/project-metadata-utils" = {
+        sha256 = "0r5dvbipcc0jhwlpaxv9iw1vrmsk1yxnycm0k4lvw1rbgikis7rv";
+        version = "1.0.2";
+      };
+
+      "elm/random" = {
+        sha256 = "138n2455wdjwa657w6sjq18wx2r0k60ibpc4frhbqr50sncxrfdl";
+        version = "1.0.0";
+      };
+
+      "elm/regex" = {
+        sha256 = "0lijsp50w7n1n57mjg6clpn9phly8vvs07h0qh2rqcs0f1jqvsa2";
+        version = "1.0.0";
+      };
+
+      "elm/time" = {
+        sha256 = "0vch7i86vn0x8b850w1p69vplll1bnbkp8s383z7pinyg94cm2z1";
+        version = "1.0.0";
+      };
+
+      "elm/virtual-dom" = {
+        sha256 = "1yvb8px2z62xd578ag2q0r5hd1vkz9y7dfkx05355iiy1d7jwq4v";
+        version = "1.0.3";
+      };
+
+      "fredcy/elm-parseint" = {
+        sha256 = "087c7p9m607p4pb9aziln3mqfczvkfm50fna5sxcsjlj45x7ibp6";
+        version = "2.0.1";
+      };
+
+      "jfmengels/elm-review" = {
+        sha256 = "171cslw6025s5i4z0vdbnpvxi01rrd6f3m5kwny9r4vx6ygfwmb5";
+        version = "2.13.0";
+      };
+
+      "mgold/elm-nonempty-list" = {
+        sha256 = "02d3xgasgy5knh02rgl403z9hwlbyrsnp9wavc2lhkxxa2misrgl";
+        version = "4.2.0";
+      };
+
+      "miniBill/elm-codec" = {
+        sha256 = "0n5zdbv6rm1nv50vc3561r2ihjb7cj0xms2ipars2sz7cl1hk8x3";
+        version = "2.0.0";
+      };
+
+      "miniBill/elm-unicode" = {
+        sha256 = "1fx9mp26kz46y88dkyd4a9jpsrz533ffjiw9243ijpx8rlap1lgm";
+        version = "1.0.3";
+      };
+
+      "noahzgordon/elm-color-extra" = {
+        sha256 = "1km72jlmgg5iqxpnvzry9wavjlwcs0n3ks8wwdiy6rj2rmzm1z20";
+        version = "1.0.2";
+      };
+
+      "robinheghan/murmur3" = {
+        sha256 = "15asmgr2zqh7rkywrg5647rpdqkpzxk02v5qc6ndj60jza3gsmjk";
+        version = "1.0.0";
+      };
+
+      "rtfeldman/elm-hex" = {
+        sha256 = "1y0aa16asvwdqmgbskh5iba6psp43lkcjjw9mgzj3gsrg33lp00d";
+        version = "1.0.0";
+      };
+
+      "stil4m/elm-syntax" = {
+        sha256 = "1dlk7gslh9la6y7y3d56a37lsymmz32rlspbywcfmq40mq9hiify";
+        version = "7.2.9";
+      };
+
+      "stil4m/structured-writer" = {
+        sha256 = "02k32yaw275bivab90wy8qkbys3gg4fw53f798dzf1j9wharhg12";
+        version = "1.0.3";
+      };
+
+      "tripokey/elm-fuzzy" = {
+        sha256 = "0v7l33fghzy910a3ck69r6lqiklprm879vhbqsmxgf7inhpsljc7";
+        version = "5.2.1";
+      };
+
+      "vito/elm-ansi" = {
+        sha256 = "0wyl7l6j48jk19vh0pzd76v4b6lg62i9ib1mzixcpnkgzp99zilv";
+        version = "10.0.1";
+      };
+
+      "zwilias/json-decode-exploration" = {
+        sha256 = "10q2xyazr67w3lnv2dza40cd52r5civg90gcn10pijh09jnmk40h";
+        version = "6.0.0";
+      };
+}
